57e01e181c30a2d7385850e732a551c08eba0ec4
Alja Mirfattahi <amirfatt@student.42.fr>
2024-07-23 16:07:40 +0200

    feat #6: first structure for input handling

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -8,21 +8,87 @@
 # include <unistd.h>
 #include "libft/libft.h"
 
+# define KEY_W 119
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define ESC 65307
+# define KEY_LEFT 65361
+# define KEY_RIGHT 65363
+
+# define EXT_ERROR	10
+# define FD_ERROR	11
+# define MAP_EMPTY	12
+
+# define PERS "FILE invalid: perspective is double...\n"
+# define PERS_M "FILE invalid: perspective is missing...\n"
+# define IDENT "FILE invalid: type identifier has more than 2 characters...\n"
+# define IDENT_M "FILE invalid: type identifier is missing...\n"
+# define FLOOR_CEILING "FILE invalid: floor/ceiling is double...\n"
+# define FL_CE_MISS "FILE invalid: floor/ceiling is missing...\n" 
+# define RGB_WRO "FILE invalid: RGB values are outside of range...\n"
+# define RGB_M "FILE invalid: RGB value missing...\n"
+
+typedef enum type_status
+{
+	VALID, // element has everything it needs; 
+	// link with macro EXT_ERROR
+	EXT_WRONG, // path has wrong extension
+	EXT_MISS, // path has no extension 
+	PERS_DOUBLE, // one/more perspective is double
+	PERS_MISS, // perspective is missing 
+	IDENT_WRONG, // type identifier has more than 2 characters
+	FLOOR_CEIL_MISS, // floor or ceiling type missing 
+	FLOOR_CEIL_DOUBLE, // floor or ceiling type double
+	RGB_WRONG, // rgb isn't 0 - 255 	
+	RGB_MISS // rgb under 3 elements
+} type_status; 
+
+typedef enum map_status
+{
+	VALID_MAP,
+	
+
+}
+
+typedef struct s_element
+{
+	char *string; 
+	char *path; 
+	char *type; 
+	int *rgb; 
+}	t_element; 
+
+typedef struct s_file 
+{
+	t_data *d; 
+	int y_axis; 
+	int x_axis;
+	t_element *e; 
+}	t_file; 
+
 typedef struct s_data
 {
 	void	*mlx;
 	void	*win;
 	int		error;
+	char **file; 
+	t_file *file; 
 }	t_data;
 
-// main.c
-int		main(void);
 
-// utils_free.c
+/*FUNCTIONS IN FILE main.c*/
+int		main(int argc, char**argv);
+
+/*FUNCTIONS IN FILE utils_free.c*/
 void	free_data(t_data *d);
 
-// init_data.c
+/*FUNCTIONS IN FILE init_data.c*/
 void	init_data(t_data *d);
+
+int valid_extension (t_data *d, char *str); 
+void get_length_of_file(t_data *d, char *argv); 
+void get_longest_line(t_data *d, char *argv); 
 
 
 #endif
--- source/main.c
+++ source/main.c
@@ -1,13 +1,18 @@
 #include "../includes/cub3d.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int		error_code;
 	t_data	*d;
 
+	error_code = 0; 
+	if(argc == 2)
+	{
 	d = (t_data *) malloc(sizeof(t_data));
 	if (d == NULL)
 		return (1);
+		
+	}
 	init_data(d);
 	error_code = d->error;
 	free_data(d);
