0fc20a35b0f5537d3d2f88afeed760abf5af749d
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-08-16 12:44:19 +0200

    chore(#12): rm executable and add structs from parsing branch

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -8,11 +8,44 @@
 # include <unistd.h>
 #include "libft/libft.h"
 
+typedef struct s_element
+{
+	char			*we_path;
+	char			*so_path;
+	char			*no_path;
+	char			*ea_path;
+	int				*ceil_rgb;
+	int				*flo_rgb;
+	struct s_file	*file;
+}	t_element;
+
+typedef struct s_map
+{
+	char			**map;
+	char			**cpy_map;
+	int				length;
+	int				width;
+	int				p_pos_y;
+	int				p_pos_x;
+	struct s_file	*file;
+} t_map;
+
+typedef struct s_file 
+{
+	struct s_data	*data; // Use Forward declaration, is only init further down
+	t_element		*elem;
+	t_map			*map;
+}	t_file; 
+
 typedef struct s_data
 {
 	void	*mlx;
-	void	*win;
+	void	*win; 
 	int		error;
+	char	**file_arr; 
+	int		y_file; 
+	int		x_file;
+	t_file	*file; 
 }	t_data;
 
 // main.c
