b8207f10e9841752d2208aa007ffe43f0e6d4baa
Luca <ca.lu39@yahoo.de>
2024-10-01 16:33:38 +0200

    fix(#32): no more segfault, by catching y values below zero (referenced wrong ticket sorry)

--- source/render_c_f.c
+++ source/render_c_f.c
@@ -16,7 +16,6 @@ void	color_below(t_data *d, double ray_hit_wall_x, double ray_hit_wall_y)
 	}
 }
 
-
 /* colors pixels from middle of screen to upper end of screen */
 void	color_above(t_data *d, int wall_height, double ray_hit_wall_x, double ray_hit_wall_y)
 {
