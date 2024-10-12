cc11ebe41113066d83cb831b82b475d155e37625
Luca <ca.lu39@yahoo.de>
2024-10-12 12:08:18 +0200

    chore(#51): norm render_column.c img_dis_col

--- source/render_column.c
+++ source/render_column.c
@@ -1,20 +1,17 @@
 #include "../includes/cub3d.h"
 
-
-
 // takes img that is to be displayed
 // takes h as in height that needs to be displayed
 // takes x and y as coordinates where raycasting meets the wall
 	// and where it should be displayed
-// startx signifies the position of x inside the img_adr that should first be accessed
 // paints the texture from the upper edge to the lower edge
 void	img_dis_col(t_data *d, double h, double x, double y)
 {
-	double quo;
-	double wall;
+	double	quo;
+	double	wall;
 	int		upper_y_pos;
-	int		source_pos;
-	int		dest_pos;
+	int		s_pos;
+	int		d_pos;
 
 	quo = d->NESW[d->elem->texnum].height / h;
 	wall = 0;
@@ -26,13 +23,12 @@ void	img_dis_col(t_data *d, double h, double x, double y)
 	}
 	while (upper_y_pos <= y && wall <= d->NESW[d->elem->texnum].height)
 	{
-		source_pos = img_get_pos(&d->NESW[d->elem->texnum], (int) ((double) d->elem->wallx * 100 * 0.63), (int) wall);
-		dest_pos = img_get_pos(d->screen, x, upper_y_pos++);
-		if (source_pos < 0 || dest_pos < 0)
-		{
+		s_pos = img_get_pos(&d->NESW[d->elem->texnum], \
+			(int)((double) d->elem->wallx * 100 * 0.63), (int) wall);
+		d_pos = img_get_pos(d->screen, x, upper_y_pos++);
+		if (s_pos < 0 || d_pos < 0)
 			break ;
-		}
-		copy_pos_to_img(d->screen, &d->NESW[d->elem->texnum], dest_pos, source_pos);
+		copy_pos_to_img(d->screen, &d->NESW[d->elem->texnum], d_pos, s_pos);
 		wall += quo;
 	}
 }
@@ -61,7 +57,7 @@ void	render_column(t_data *d, int x)
 			img_dis_col(d, wall_height, x + loop, ray_hit_wall_y);
 		if (d->elem->line_height < SCREEN_H || d->ray->hit == 0)
 			color_below(d, x + loop, ray_hit_wall_y);
-		loop++;		
+		loop++;
 	}
 	return ;
 }
