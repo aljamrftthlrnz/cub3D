558b86dae57a6ca9d05bcd53192a931c4bea4b98
Luca <ca.lu39@yahoo.de>
2024-10-12 11:51:05 +0200

    chore(#51): norm and shorten render_column.c

--- source/render_column.c
+++ source/render_column.c
@@ -19,7 +19,7 @@ void	img_dis_col(t_data *d, t_image *img, double h, double x, double y, int star
 
 	quo = img->height/h;
 	wall = 0;
-	upper_y_pos = y - h; //+ 1; 
+	upper_y_pos = y - h;
 	if (upper_y_pos < 0)
 	{
 		wall = upper_y_pos * (-1) / 2 / h * 64;
@@ -36,65 +36,40 @@ void	img_dis_col(t_data *d, t_image *img, double h, double x, double y, int star
 			printf("skips at x==%f y==%f //startx==%d wall==%f...s==%d d==%d\n", x, y, startx, wall, source_pos, dest_pos);
 			break ;
 		}
-		// printf("rgb: %d %d %d\n", img->img_adr[source_pos], img->img_adr[source_pos + 1], img->img_adr[source_pos + 2]);
 		copy_pos_to_img(d->screen, img, dest_pos, source_pos);
 		wall += quo;
-		// upper_y_pos++;
 	}
 
 }
 
-// this function will take the raycasting struct and render entire column
+// this function will take values from raycasting struct and render entire column
 	// texture (N)
 	// floor and ceiling
 	// correct width
 void	render_column(t_data *d, int x)
 {
-	int column_width = LINE_W; //delete/replace later with actual width from struct or define
-
-	/* coordinates where ray hits the wall */
-	int	ray_hit_wall_x = x; //delete/replace
-	int	ray_hit_wall_y = d->elem->drawEnd; //delete/replace
-	// int	ray_hit_wall_y = (SCREEN_H - d->elem->line_height) / 2 + d->elem->line_height; //delete/replace
-	/* wall/line height */
-	int	wall_height = d->elem->line_height; //delete/replace
-	// int	wall_height = d->ray->perpWallDist; //delete/replace
-
-
-	/* this is the x coordinate of the texture */
-	/* expected input: zero means on the left most, 32 in the middle, 63 is the right most */
-	double	texture_segment = d->elem->wallx * 100 * 0.63; //* 64
+	int	ray_hit_wall_y;
+	int	wall_height;
 	int	loop;
 
-	// color_above(d, wall_height, ray_hit_wall_x, ray_hit_wall_y);
-	// color_below(d, ray_hit_wall_x, ray_hit_wall_y);
-	// if (d->ray->hit == 1)
-	// {
-	// 	double	texture_segment = d->elem->wallx * 100 * 0.63;
-	// 	img_dis_col(d, &d->NESW[d->elem->texnum], wall_height, ray_hit_wall_x, ray_hit_wall_y, (int) (texture_segment));
-	// }
-
-
 	loop = 0;
-	
-	/* adjusting some variables for "no wall hit" */
+	ray_hit_wall_y = d->elem->drawEnd;
+	wall_height = d->elem->line_height;
 	if (d->ray->hit == 0)
 	{
 		wall_height = 0;
 		ray_hit_wall_y = SCREEN_H / 2;
 	}
-	while (loop < column_width)
+	while (loop < LINE_W)
 	{
 		if (d->elem->line_height < SCREEN_H || d->ray->hit == 0)
-			color_above(d, wall_height, ray_hit_wall_x + loop, ray_hit_wall_y);
+			color_above(d, wall_height, x + loop, ray_hit_wall_y);
 		if (d->ray->hit == 1)
-		{
-			img_dis_col(d, &d->NESW[d->elem->texnum], wall_height, ray_hit_wall_x + loop, ray_hit_wall_y, (int) (texture_segment));
-		}
+			img_dis_col(d, &d->NESW[d->elem->texnum], wall_height, x + loop, \
+				ray_hit_wall_y, (int) d->elem->wallx * 100 * 0.63);
 		if (d->elem->line_height < SCREEN_H || d->ray->hit == 0)
-			color_below(d, ray_hit_wall_x + loop, ray_hit_wall_y);
+			color_below(d, x + loop, ray_hit_wall_y);
 		loop++;
-		
 	}
 	return ;
 }
