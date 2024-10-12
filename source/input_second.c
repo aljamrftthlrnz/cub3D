fd67d94e5d2185e3ac0eb27c96afe06d6f356fa5
Luca <ca.lu39@yahoo.de>
2024-10-12 11:24:38 +0200

    chore(#50): norm input_second.c

--- source/input_second.c
+++ source/input_second.c
@@ -1,9 +1,9 @@
 #include "../includes/cub3d.h"
 
-int check_multiple_seperators(char *str)
+int	check_multiple_seperators(char *str)
 {
-	int sep;
-	int i;
+	int	sep;
+	int	i;
 
 	i = 0; 
 	sep = 0;
@@ -22,16 +22,16 @@ int check_multiple_seperators(char *str)
 	return (sep);
 }
 
-int check_order_of_map(t_data *d)
+int	check_order_of_map(t_data *d)
 {
-	int length;
-	int j; 
+	int	length;
+	int	j;
 
 	length = file_length(d->file_arr); 
-	j = length - 1; 
-	while(!is_space(d->file_arr[j]))
-		j--; 
-	if(is_map_line(d->file_arr[j]))
-		return(1); 
-	return (0); 
+	j = length - 1;
+	while (!is_space(d->file_arr[j]))
+		j--;
+	if (is_map_line(d->file_arr[j]))
+		return (1);
+	return (0);
 }
