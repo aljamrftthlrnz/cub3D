8ab6cc5ba7412dce9df97a3069206cb6a990a75d
Alja Mirfattahi <118297608+aljamrftthlrnz@users.noreply.github.com>
2024-10-17 19:28:33 +0200

    Revert "Feat 55"

--- source/input_first.c
+++ source/input_first.c
@@ -1,5 +1,32 @@
 #include "../includes/cub3d.h"
 
+char	**setup_rgb_values(t_data *d, char *str, char *ptr)
+{
+	if (check_multiple_seperators(str))
+	{
+		free(ptr);
+		err_free_message(d, FL_CEIL_M);
+	}
+	return (ft_split(str, ','));
+}
+
+void	rgb_null_check(t_data *d, char **rgb_values, int *rgb, char *ptr)
+{
+	if (!rgb_values || !rgb || file_length(rgb_values) != 3)
+	{
+		free (ptr);
+		if (!rgb_values || !rgb)
+		{
+			free(rgb);
+			free_array(rgb_values);
+			err_free_message(d, ALLOC_FAIL);
+		}
+		free(rgb);
+		free_array(rgb_values);
+		err_free_message(d, FL_CEIL_M);
+	}
+}
+
 int	*parse_rgb_colors(char *str, t_data *d, char *ptr)
 {
 	char	**rgb_values;
@@ -8,14 +35,6 @@ int	*parse_rgb_colors(char *str, t_data *d, char *ptr)
 	rgb_values = setup_rgb_values(d, str, ptr);
 	rgb = (int *) malloc(sizeof(int) * 3);
 	rgb_null_check(d, rgb_values, rgb, ptr);
-	if (!is_valid_rgb(rgb_values[0]) || !is_valid_rgb(rgb_values[1]) \
-		|| !is_valid_rgb(rgb_values[2]))
-	{
-		free_array(rgb_values);
-		free (rgb);
-		free(ptr);
-		err_free_message(d, RGB_MI);
-	}
 	rgb[0] = ft_atoi(rgb_values[0]);
 	rgb[1] = ft_atoi(rgb_values[1]);
 	rgb[2] = ft_atoi(rgb_values[2]);
